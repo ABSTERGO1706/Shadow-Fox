7ccdde625494a431040d6f5cd05b3e852adcfe63
Hari Roshan <hariroshan17@gmail.com>
2026-03-23 18:44:05 +0530

    Delete CSK_FAN_PAGE directory

--- CSK_FAN_PAGE/backend/src/main/java/com/example/IPL_Fan_Page/TimelineRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.IPL_Fan_Page;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TimelineRepository extends JpaRepository<Timeline,Integer> {
-}

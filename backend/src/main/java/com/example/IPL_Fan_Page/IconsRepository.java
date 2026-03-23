fd02762f187c4b314e0e80a93bd895781f4d26ed
Hari Roshan <hariroshan17@gmail.com>
2026-03-23 19:18:52 +0530

    Delete backend/src directory

--- backend/src/main/java/com/example/IPL_Fan_Page/IconsRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.IPL_Fan_Page;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IconsRepository extends JpaRepository<Icons , Integer> {
-}
--- backend/src/main/java/com/example/IPL_Fan_Page/PlayerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.IPL_Fan_Page;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerRepository extends JpaRepository<Player , Integer> {
-}
